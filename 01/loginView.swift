7dc14ad40bc5b1cfd3e4e2d925b20dc4f5d4686d
zhlhu322 <0322enter@gmail.com>
2025-04-28 01:37:10 +0800

    0428

--- 01/loginView.swift
+++ 01/loginView.swift
@@ -15,35 +15,72 @@ struct loginView: View {
     @State var password:String = ""
     
     var body: some View {
-        Text("建立帳戶")
-            .font(.title)
-            .foregroundStyle(Color(.mint))
-            .frame(width:500,height:250)
-            .background(Color(.darkBackground))
-            .padding(.bottom,30)
         
-        VStack(spacing:15){
-            TextField("姓名",text: $name)
-                .padding()
-                .frame(width:350,height:60)
-                .background(Color(.black.opacity(0.05)))
-                .cornerRadius(10)
-                
-                        
-            TextField("姓名",text: $name)
-                .padding()
-                .frame(width:350,height:60)
-                .background(Color(.black.opacity(0.05)))
-                .cornerRadius(10)
-                    
-            TextField("姓名",text: $name)
-                .padding()
-                .frame(width:350,height:60)
-                .background(Color(.black.opacity(0.05)))
-                .cornerRadius(10)
+        ZStack(alignment: .bottomLeading) {
+            Color(.darkBackground).ignoresSafeArea()
+            
+            Text("建立帳戶")
+                .font(.title)
+                .foregroundStyle(Color(.mint))
+                .padding(.bottom,25)
+                .padding(.horizontal,20)
         }
-        
-        
+        .frame(maxWidth: .infinity)
+        .frame(height:200)
+        .padding(.bottom,-10)
+
+        VStack(spacing:15){
+            HStack(spacing:15) {
+                Image(systemName:"person.circle.fill")
+                    .foregroundStyle(Color(.darkBackground).opacity(0.5))
+                    .frame(width:20)
+                TextField("姓名",text: $name)
+                    .foregroundStyle(Color(.darkBackground).opacity(0.5))
+            }
+            .padding()
+            .frame(width:350,height:60)
+            .background(Color(.background))
+            .cornerRadius(10)
+                
+            HStack(spacing:15) {
+                Image(systemName:"envelope.fill")
+                    .foregroundStyle(Color(.darkBackground).opacity(0.5))
+                    .frame(width:20)
+                TextField("電子信箱",text: $mail)
+            }
+            .padding()
+            .frame(width:350,height:60)
+            .background(Color(.background))
+            .cornerRadius(10)
+                    
+            HStack(spacing:15) {
+                Image(systemName:"key.fill")
+                    .foregroundStyle(Color(.darkBackground).opacity(0.5))
+                    .frame(width:20)
+                SecureField("密碼",text: $password)
+            }
+            .padding()
+            .frame(width:350,height:60)
+            .background(Color(.background))
+            .cornerRadius(10)
+            
+            
+            Button("登入"){
+                
+            }
+            .frame(width:350,height:60)
+            .foregroundStyle(Color(.white))
+            .background(Color(.primary))
+            .cornerRadius(20)
+            .padding(.top,50)
+            
+            Spacer()
+            
+        }
+        .ignoresSafeArea()
+        .frame(maxWidth:.infinity,maxHeight:.infinity)
+        .padding(.top,50)
+        .background(Color(.mint))
         
         Spacer()
     }
