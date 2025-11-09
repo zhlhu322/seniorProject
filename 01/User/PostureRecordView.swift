1401fbae418c534f9a551ed5f36c1761da2ac9d4
Enya <0322enter@gmail.com>
2025-11-09 23:20:06 +0800

    Add PostureRecordView

---
+++ 01/User/PostureRecordView.swift
@@ -0,0 +1,18 @@
+//
+//  PostureRecordView.swift
+//  01
+//
+//  Created by 李恩亞 on 2025/11/8.
+//
+
+import SwiftUI
+
+struct PostureRecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostureRecordView()
+}
